c9f0d5d55e9e8e534e5948dd2c96d4f036f169e3
Carlos <carlos.prieto@cunef.edu>
2025-11-15 12:03:48 +0100

    Añadir cuenta.cpp y cuenta.hpp antes del merge

---
+++ cuenta.hpp
@@ -0,0 +1,30 @@
+#ifndef CARTERA_HPP
+#define CARTERA_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Transaccion.hpp"
+#include "Producto.hpp"
+#include "PreciosHistoricos.hpp"
+
+// Declaración anticipada de Cuenta para evitar dependencias circulares y usarla en comprarProducto
+class Cuenta;
+
+using namespace std;
+
+class Cartera {
+private:
+    string nombre;
+    vector<Transaccion> transacciones;
+
+public:
+    // Constructores
+    Cartera();
+    Cartera(string nombre);
+    
+    // Getter
+    string getNombre() const { return nombre; } // Añadido getter para usar en Cuenta::mostrar
+}
+
+#endif
